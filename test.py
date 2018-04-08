45e3662504b579d4f324c5b66a956191c829a513
Nick Krumholz <nick.krumholz@gmail.com>
2018-04-08 16:01:47 -0700

    Added some files to gitignore. Made email/text functionality reusable by making it an import. Added logging functionality and removed print methods throughout.

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import logging
 import requests
 import pandas
 import credentials
 import numpy
-import smtplib
 from datetime import datetime
+import messageMyself
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(filename='errorLog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.INFO)
+logging.debug('Start of program')
 
 
 def get_square_items(url='', creds=credentials):
@@ -20,9 +26,18 @@ def get_square_items(url='', creds=credentials):
 	data = r.json()
 	for d in data:
 		temp = []
-		temp.append(d['variations'][0]['id'])
-		temp.append(d['name'])
-		i.append(temp)
+		if len(d['variations']) > 1:
+			for each in d['variations']:
+				temp2 = []
+				temp2.append(each['id'])
+				temp2.append(d['name'] + ' {' + each['name'] + '}')
+				temp2.append(each['track_inventory'])
+				i.append(temp2)
+		else:
+			temp.append(d['variations'][0]['id'])
+			temp.append(d['name'])
+			temp.append(d['variations'][0]['track_inventory'])
+			i.append(temp)
 
 	if r.headers.get('Link') is not None:
 		next_page = r.headers.get('Link')
@@ -97,34 +112,47 @@ def get_bigcommerce_inventory(next_page='', creds=credentials):
 i = []
 q = []
 b = []
+
 square_items = get_square_items()
+df1 = pandas.DataFrame(square_items, columns=['iD', 'name', 'squareTracking'])
+# print(df1)
 square_quantity = get_square_quantity()
-bigcommerce_inventory = get_bigcommerce_inventory()
-df1 = pandas.DataFrame(square_items, columns=['iD', 'name'])
 df2 = pandas.DataFrame(square_quantity, columns=['iD', 'squareQuantity'])
-df3 = pandas.DataFrame(bigcommerce_inventory, columns=['name', 'bigCommerceQuantity'])
 df_square_combine = pandas.merge(df1, df2, on='iD', how='outer')
-df_square_bigcommerce = pandas.merge(df_square_combine, df3, on='name', how='outer')
-np_diff = numpy.where(df_square_bigcommerce['squareQuantity'] != df_square_bigcommerce['bigCommerceQuantity'], df_square_bigcommerce['name'], '')
-df_diff = pandas.DataFrame(np_diff, columns=['name'])
-df_merged = pandas.merge(df_diff, df_square_bigcommerce, on='name', how='inner')
 
-difference = ''
-for index, row in df_merged.iterrows():
-	difference = row[1] + ', ' + row[0] + ', ' + str(row[2]) + ', ' + str(row[3]) + '\n' + difference
-print(difference)
+log = {}
+square_qty_nan = df_square_combine['squareQuantity'].isna().sum()
+log['sqQtyNan'] = square_qty_nan
+square_tracking_qty = df_square_combine['squareTracking'].value_counts()
+log['sqTrackingQtyFalse'] = square_tracking_qty[False]
+square_qty_not_nan = df_square_combine['squareQuantity'].count()
+log['sqQtyNotNan'] = square_qty_not_nan
+log['sqTrackingQtyTrue'] = square_tracking_qty[True]
+logging.info('%s', log)
 
-today = datetime.now()
-today = today.strftime("%B %d, %Y %I:%M%p - %A")
-message = 'Subject: Inventory Results (' + today + ')\nData...\n' + difference
-message = message.encode("utf8")
-email_from = credentials.email['email']
-email_to = 'nick.krumholz@gmail.com'
-email_pass = credentials.email['password']
+# if log['sqNanCount'] == log['sqTrackingFalseCount']:
+# 	logging.info('The number of items with Square inventory tracking turned off is: %s.', log['sqNanCount'])
+# else:
+# 	logging.info('There is a discrepenacy in the number of inventory items being tracked in Square. Square Nan Quantity = %s. Square Tracking Quantity = %s.', log['sqNanCount'], log['sqTrackingFalseCount'])
 
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login(email_from, email_pass)
-smtpObj.sendmail(email_from, email_to, message)
-smtpObj.quit()
+# logging.info('The total number of items in Square Tracking is: %s.', log['squareTrackingTotalCount'])
+
+
+# bigcommerce_inventory = get_bigcommerce_inventory()
+# df3 = pandas.DataFrame(bigcommerce_inventory, columns=['name', 'bigCommerceQuantity'])
+# df_square_bigcommerce = pandas.merge(df_square_combine, df3, on='name', how='outer')
+# np_diff = numpy.where(df_square_bigcommerce['squareQuantity'] != df_square_bigcommerce['bigCommerceQuantity'], df_square_bigcommerce['name'], '')
+# df_diff = pandas.DataFrame(np_diff, columns=['name'])
+# df_merged = pandas.merge(df_diff, df_square_bigcommerce, on='name', how='inner')
+#
+# difference = ''
+# for index, row in df_merged.iterrows():
+# 	difference = str(row[1]) + ', ' + str(row[0]) + ', ' + str(row[2]) + ', ' + str(row[3]) + '\n' + difference
+# print(difference)
+#
+# today = datetime.now()
+# today = today.strftime("%B %d, %Y %I:%M%p - %A")
+# message = 'Subject: Inventory Results (' + today + ')\nData...\n\n' + difference
+# messageMyself.emailMyself(message)
+
+logging.debug('End of program')
