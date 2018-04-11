f236edfa7c54996268bc0230897a97bef834f0a4
Nick Krumholz <nick.krumholz@gmail.com>
2018-04-11 08:51:35 -0700

    Removed some unnecessary code and turned on email messaging.

--- test.py
+++ test.py
@@ -149,18 +149,9 @@ for index, row in dfWithoutTrackingMismatches.iterrows():
 message = message + messageLoop
 logging.info('%s', message)
 
-# np_diff = numpy.where(df_square_bigcommerce['squareQuantity'] != df_square_bigcommerce['bigCommerceQuantity'], df_square_bigcommerce['name'], '')
-# df_diff = pandas.DataFrame(np_diff, columns=['name'])
-# df_merged = pandas.merge(df_diff, df_square_bigcommerce, on='name', how='inner')
-#
-# difference = ''
-# for index, row in df_merged.iterrows():
-# 	difference = str(row[1]) + ', ' + str(row[0]) + ', ' + str(row[2]) + ', ' + str(row[3]) + '\n' + difference
-# print(difference)
-#
-# today = datetime.now()
-# today = today.strftime("%B %d, %Y %I:%M%p - %A")
-# message = 'Subject: Inventory Results (' + today + ')\nData...\n\n' + difference
-# messageMyself.emailMyself(message)
+today = datetime.now()
+today = today.strftime("%B %d, %Y %I:%M%p - %A")
+message = 'Subject: Inventory Results (' + today + ')\nData...\n\n' + message
+messageMyself.emailMyself(message)
 
 logging.debug('End of program')
