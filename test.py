45c9f49868ca8e3c4c48d2696feda01b568d088c
Nick Krumholz <nick.krumholz@gmail.com>
2018-04-12 19:34:13 -0700

    Made the final edits to the script and ran it a few times to work through some minor bugs. Added a cron job to my local machine to make it run and worked through a few bugs there as well.

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import numpy
 from datetime import datetime
 import messageMyself
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-# logging.basicConfig(filename='errorLog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='/Applications/MAMP/htdocs/square-bigcommerce/errorLog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # logging.disable(logging.INFO)
 logging.debug('Start of program')
 
@@ -21,6 +21,7 @@ def get_square_items(url='', creds=credentials):
 		url = 'https://connect.squareup.com/v1/' + square_location + '/items'
 
 	r = requests.get(url, headers={'Authorization': 'Bearer ' + square_bearer_token, 'Accept': 'application/json'})
+	logging.info('%s', r.headers)
 	data = r.json()
 	for d in data:
 		temp = []
@@ -52,6 +53,7 @@ def get_square_quantity(url='', creds=credentials):
 		url = 'https://connect.squareup.com/v1/' + square_location + '/inventory'
 
 	r = requests.get(url, headers={'Authorization': 'Bearer ' + square_bearer_token, 'Accept': 'application/json'})
+	logging.info('%s', r.headers)
 	data = r.json()
 	for d in data:
 		temp = []
@@ -82,19 +84,31 @@ def get_bigcommerce_inventory(next_page='', creds=credentials):
 	else:
 		paginate = next_page
 	# url = 'https://api.bigcommerce.com/stores/' + credentials.bigcommerce['store'] + '/v3/catalog/products?include_fields=name,inventory_level&page=' + str(paginate) + '&limit=250'
-	url = 'https://api.bigcommerce.com/stores/' + credentials.bigcommerce['store'] + '/v3/catalog/products?page=' + str(paginate) + '&limit=250'
+	url = 'https://api.bigcommerce.com/stores/' + credentials.bigcommerce['store'] + '/v3/catalog/products?page=' + str(paginate) + '&limit=250&include=variants&include_fields=name,inventory_level,inventory_tracking,current_page,total_pages,label'
 
 	r = requests.get(url, headers=headers)
+	logging.info('%s', r.headers)
 	data = r.json()
 	for d in data['data']:
 		temp = []
-		temp.append(d['name'])
-		temp.append(d['inventory_level'])
-		if d['inventory_tracking'] == 'none':
-			temp.append(False)
+		if d['variants'][0]['option_values']:
+			for each in d['variants']:
+				temp2 = []
+				temp2.append(d['name'] + ' {' + each['option_values'][0]['label'] + '}')
+				temp2.append(each['inventory_level'])
+				if d['inventory_tracking'] == 'none':
+					temp2.append(False)
+				else:
+					temp2.append(d['inventory_tracking'])
+				b.append(temp2)
 		else:
-			temp.append(d['inventory_tracking'])
-		b.append(temp)
+			temp.append(d['name'])
+			temp.append(d['inventory_level'])
+			if d['inventory_tracking'] == 'none':
+				temp.append(False)
+			else:
+				temp.append(d['inventory_tracking'])
+			b.append(temp)
 	if data['meta']['pagination']['current_page'] != data['meta']['pagination']['total_pages']:
 		next_page = data['meta']['pagination']['current_page'] + 1
 		get_bigcommerce_inventory(next_page=next_page)
@@ -136,22 +150,30 @@ log['sqTrackingQtyTrue'] = square_tracking_qty[True]
 log['bcTrackingQtyProduct'] = bcTracking['product']
 logging.info('%s', log)
 
-message = 'In Square but not in BigCommerce: ' + str(squareUniques.shape[0]) + ' items\n'
-message = message + '\n'.join(squareUniques['name'])
-message = message + '\n\nIn BigCommerce but not in Square: ' + str(bcUniques.shape[0]) + ' items\n'
-message = message + '\n'.join(bcUniques['name'])
-message = message + '\n\nInventory tracking that does not match in Square or BigCommerce: ' + str(sqTrackingFalse.shape[0] + bcTrackingFalse.shape[0]) + ' items\n'
-message = message + '\n'.join(sqTrackingFalse['name']) + '\n' + '\n'.join(bcTrackingFalse['name'])
-message = message + '\n\nInventory quantities that do not match: ' + str(dfWithoutTrackingMismatches.shape[0]) + ' items\n'
+message = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>Inventory Results</title><style type="text/css"></style></head><body style="margin:0; padding:0; background-color:#FFFFFF; padding:20px;"><center><table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF"><tr><td align="center" valign="top">This report is for informational and troubleshooting purposes and should be used as a tool to determine if items listed here require updates. For example, items that are added directly to Square for the sake of variable pricing in-store, will appear in the first section but will not need corrective action.</td></tr>'
+message = message + '<tr><td>&nbsp;</td></tr><tr><td><h3>In Square but not in BigCommerce: ' + str(squareUniques.shape[0]) + ' Items</h3></td></tr>'
+for index, row in squareUniques.iterrows():
+    message = message + '<tr><td>' + row['name'] + '</td></tr>'
+message = message + '<tr><td>&nbsp;</td></tr><tr><td><h3>In BigCommerce but not in Square: ' + str(bcUniques.shape[0]) + ' Items</h3></td></tr>'
+for index, row in bcUniques.iterrows():
+    message = message + '<tr><td>' + row['name'] + '</td></tr>'
+message = message + '<tr><td>&nbsp;</td></tr><tr><td><h3>Inventory Tracking settings that do not Match: ' + str(sqTrackingFalse.shape[0] + bcTrackingFalse.shape[0]) + ' Items</h3></td></tr>'
+for index, row in sqTrackingFalse.iterrows():
+    message = message + '<tr><td>' + row['name'] + '</td></tr>'
+for index, row in bcTrackingFalse.iterrows():
+    message = message + '<tr><td>' + row['name'] + '</td></tr>'
+message = message + '<tr><td>&nbsp;</td></tr><tr><td><h3>Inventory quantities that do not match: ' + str(dfWithoutTrackingMismatches.shape[0]) + ' Items</h3></td></tr>'
 messageLoop = ''
 for index, row in dfWithoutTrackingMismatches.iterrows():
-	messageLoop = messageLoop + row['name'] + ' ' + str(row['squareQuantity']) + ' ' + str(row['bigCommerceQuantity']) + '\n'
+	messageLoop = messageLoop + '<tr><td>' + row['name'] + ' ' + str(row['squareQuantity']) + ' ' + str(row['bigCommerceQuantity']) + '</td></tr>'
 message = message + messageLoop
+message = message + '</table></center></body></html>'
+# message = message.encode("utf8")
 logging.info('%s', message)
 
 today = datetime.now()
 today = today.strftime("%B %d, %Y %I:%M%p - %A")
-message = 'Subject: Inventory Results (' + today + ')\nData...\n\n' + message
-messageMyself.emailMyself(message)
+subject = 'Inventory Results (' + today + ')'
+messageMyself.emailMyself(subject, message)
 
 logging.debug('End of program')
